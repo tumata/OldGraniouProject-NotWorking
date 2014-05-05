74da645c1124f2422c99614b07790bf3053cdad6
Philippe Bertin <philippe.t.bertin@gmail.com>
2014-05-05 18:56:57 +0200

    Initial Commit

---
+++ GraniouProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GraniouProject
+//
+//  Created by Philippe Tumata on 05/05/2014.
+//  Copyright (c) 2014 telecom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GraniouProject/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GraniouProject
+//
+//  Created by Philippe Tumata on 05/05/2014.
+//  Copyright (c) 2014 telecom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
